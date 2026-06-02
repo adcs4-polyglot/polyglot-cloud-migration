66f148808b53873ea8fdf9e11096dac0b45df7f0
Mohammad Rafiq <243424@students.au.edu.pk>
2026-06-03 04:46:51 +0500

    feat: cloud VM created on Azure with Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -22,6 +22,7 @@ resource "azurerm_public_ip" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "main" {
